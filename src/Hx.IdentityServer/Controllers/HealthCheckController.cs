3f299649a966dd404597977f7e296be631824440
songtaojie <stjworkemail@163.com>
2021-10-23 15:31:27 +0800

    添加健康检查

---
+++ src/Hx.IdentityServer/Controllers/HealthCheckController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hx.IdentityServer.Controllers
+{
+    public class HealthCheckController : Controller
+    {
+        private readonly ILogger<HealthCheckController> _logger;
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public HealthCheckController(ILogger<HealthCheckController> logger)
+        {
+            _logger = logger;
+        }
+        /// <summary>
+        /// 健康检查接口
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("~/api/[controller]")]
+        public IActionResult Get()
+        {
+            return Ok("ok");
+        }
+
+
+        /// <summary>
+        /// elk日志测试
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IEnumerable<string> GetLog()
+        {
+            _logger.LogInformation("这是一条测试日志信息");
+            _logger.LogWarning("这是一条测试日志信息Warn");
+            _logger.LogError("这是一条测试日志信息Error");
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
